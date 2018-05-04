e00c9126e752b7425de6e3c0405be7bcd7c053f6
Hector Partidas <hpartidas@deuz.net>
2018-05-04 15:52:30 -0500

    Feedback and tips from Bob
    Signed-off-by: Hector Partidas <hpartidas@deuz.net>

--- JS-StringCalculator/test/Calculator.spec.js
+++ JS-StringCalculator/test/Calculator.spec.js
@@ -6,43 +6,68 @@ const c = new Calculator();
 
 describe("Calculator", () => {
   describe("#add", () => {
-    describe("when input is empty", () => {
-      it("is expected to be 0", () => {
-        expect(c.add("")).to.equal(0);
+    let input;
+
+    function theCalculation() {
+      c.add(input);
+    }
+
+    it('returns a Promose', () => {
+
+    });
+
+    describe('the returned Promise', () => {
+      context('given X', () => {
+        it('does Y when given X');
+      });
+
+      context('given Y', () => {});
+    });
+
+    context("when input is empty", () => {
+      beforeEach('set input', () => {
+        input = "";
+      });
+
+      it("is 0", () => {
+        const expected = 0;
+        expect(theCalculation()).to.equal(expected);
       });
     });
 
-    describe("when input is 1 number", () => {
+    context("when input is 1 number", () => {
       it("is expected to be the same number", () => {
         expect(c.add("5")).to.equal(5);
       });
+
+      it('something else');
     });
 
-    describe("when input has an unknown number of numbers", () => {
+    context("when input has an unknown number of numbers", () => {
       it("is expected to succeed", () => {
         expect(c.add("1,2,3,4,5,6,7,8,9,10,11,12,13")).to.equal(91);
       });
     });
 
-    describe("when input has 2 numbers", () => {
+    context("when input has 2 numbers", () => {
       it("is expected to return the sum of the input", () => {
         expect(c.add("2,10")).to.equal(12);
       });
     });
 
-    describe("when input contains new line as delimiter", () => {
+    context("when input contains new line as delimiter", () => {
       it("is expected to return the sum of the input", () => {
         expect(c.add("2\n10,13")).to.equal(25);
       });
     });
 
-    describe("when input contains different delimiters", () => {
+    context("when input contains different delimiters", () => {
       it("is expected to return the sum of the input", () => {
         expect(c.add("1|2|3,4;\n5\n6")).to.equal(21);
       });
     });
 
-    describe("when input contains numbers greater than 1000", () => {
+    context("when input contains numbers greater than 1000", () => {
       it("is expected to return the sum of numbers lesser than 1000", () => {
         expect(c.add("100,200,1001,100,200,1001")).to.equal(600);
       });
